be98b5b21e7c963380429962e6d743358d8533cc
IMadeThisJustToPostThis <matzielbiz@gmail.com>
2022-10-27 19:25:05 -0400

    changed seeds

--- db/schema.sql
+++ db/schema.sql
@@ -1,2 +1,14 @@
 DROP DATABASE IF EXISTS listmaker_db; 
-CREATE DATABASE listmaker_db 
+CREATE DATABASE listmaker_db;
+
+USE listmaker_db;
+
+create table lists (
+    list_id INT NOT NULL,
+    list_title VARCHAR(30) NOT NULL,
+    list_type VARCHAR(30) NOT NULL,
+    list_owner VARCHAR(30) NOT NULL,
+    list_contents TEXT[],
+    list_comments TEXT[],
+    list_date VARCHAR(30) NOT NULL
+)
